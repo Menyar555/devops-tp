df5bae3ee99bdf06b09f4503f7e252f40d848e57
Menyar555 <165448123+Menyar555@users.noreply.github.com>
2025-03-07 02:15:06 +0100

    Update test_app.py

--- hello-devops/test_app.py
+++ hello-devops/test_app.py
@@ -10,5 +10,10 @@ class TestApp(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data.decode('utf-8'), "Hello, CI/CD!")
 
+    def test_status_route(self):
+        response = self.tester.get('/status')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data.decode('utf-8'), "App is running!")
+
 if __name__ == "__main__":
     unittest.main()
